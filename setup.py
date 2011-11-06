836cf27925937a74e2ce2f9b6234610e429d7ca5
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2011-11-06 05:43:05 -0200

    Initial structure

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='envparser',
+      version=version,
+      description="Environment parser",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='python environment parser configuration settings',
+      author='Diogo Baeder',
+      author_email='desenvolvedor@diogobaeder.com.br',
+      url='https://github.com/diogobaeder/envparser',
+      license='BSD 2-Clause',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )

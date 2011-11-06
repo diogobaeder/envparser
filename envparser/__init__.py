458ac836091846bd46d724ae218ad866d7670de1
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2011-11-06 16:19:59 -0200

    additional envs; additional types

--- envparser/__init__.py
+++ envparser/__init__.py
@@ -1,11 +1,34 @@
 #
-from ConfigParser import RawConfigParser
+import ConfigParser
+import os
 
 
 class Parser(object):
-    def __init__(self, basefile):
-        self.__parser = RawConfigParser()
-        self.__parser.read(basefile)
+    def __init__(self, basefile, environment='DEFAULT'):
+        self.__parser = ConfigParser.SafeConfigParser()
+        if environment == 'DEFAULT':
+            self.__parser.read(basefile)
+        else:
+            self.__parser.read([basefile, self.__environment_file_from(basefile, environment)])
+        self.environment = environment
 
-    def get(self, section, option):
-        return self.__parser.get(section, option)
+    def __environment_file_from(self, basefile, environment):
+            extension = os.path.splitext(basefile)[1]
+            directory = os.path.dirname(basefile)
+            return os.path.join(directory, environment + extension)
+
+    def __get_with_method(self, method, option):
+        try:
+            return method(self.environment, option)
+        except ConfigParser.NoSectionError:
+            return method('DEFAULT', option)
+
+
+    def get(self, option):
+        return self.__get_with_method(self.__parser.get, option)
+
+    def getint(self, option):
+        return self.__get_with_method(self.__parser.getint, option)
+
+    def getfloat(self, option):
+        return self.__get_with_method(self.__parser.getfloat, option)
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -14,6 +14,70 @@ class ParserTest(TestCase):
     def gets_string_from_configuration(self):
         parser = Parser(self.basefile)
 
-        name = parser.get('section1', 'name')
+        name = parser.get('name')
 
         self.assertEqual(name, 'John Doe')
+
+    @istest
+    def gets_string_from_configuration_default_if_section_and_file_dont_exist(self):
+        parser = Parser(self.basefile, 'weirdsection')
+
+        name = parser.get('name')
+
+        self.assertEqual(name, 'John Doe')
+
+    @istest
+    def gets_string_from_another_environment_if_section_exists(self):
+        parser = Parser(self.basefile, 'dev')
+
+        name = parser.get('name')
+
+        self.assertEqual(name, 'John Doe Dev')
+
+    @istest
+    def gets_string_from_another_environment_if_file_exists(self):
+        parser = Parser(self.basefile, 'live')
+
+        name = parser.get('name')
+
+        self.assertEqual(name, 'John Doe Live')
+
+    @istest
+    def gets_string_from_another_environment_if_file_and_section_exists(self):
+        parser = Parser(self.basefile, 'tests')
+
+        name = parser.get('name')
+
+        self.assertEqual(name, 'John Doe Tests')
+
+    @istest
+    def gets_integer_from_configuration(self):
+        parser = Parser(self.basefile)
+
+        age = parser.getint('age')
+
+        self.assertEqual(age, 30)
+
+    @istest
+    def gets_integer_from_configuration_default_if_doesnt_exist_in_section(self):
+        parser = Parser(self.basefile, 'dev')
+
+        age = parser.getint('age')
+
+        self.assertEqual(age, 30)
+
+    @istest
+    def gets_integer_from_configuration_default_if_section_and_file_dont_exist(self):
+        parser = Parser(self.basefile, 'weirdsection')
+
+        name = parser.getint('age')
+
+        self.assertEqual(name, 30)
+
+    @istest
+    def gets_float_from_configuration(self):
+        parser = Parser(self.basefile)
+
+        salary = parser.getfloat('salary')
+
+        self.assertEqual(salary, 560.00)
